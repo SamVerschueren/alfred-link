f9e4b9593223a6ab8e8e8de2ea492d54d9944bc3
Sam Verschueren <sam.verschueren@gmail.com>
2016-08-26 21:51:56 +0200

    resolve alfred prefs

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
-const userHome = require('user-home');
 const pathExists = require('path-exists');
 const pkgUp = require('pkg-up');
 const pify = require('pify');
 const del = require('del');
+const resolveAlfredPrefs = require('resolve-alfred-prefs');
 const plistTransform = require('./lib/plist-transform');
 
 const fsP = pify(fs);
-const workflowDir = path.join(userHome, 'Library/Application Support/Alfred 3/Alfred.alfredpreferences/workflows');
 
 module.exports = () => {
 	const cwd = process.cwd();
 
-	return pathExists(workflowDir)
+	let workflowDir;
+
+	return resolveAlfredPrefs()
+		.then(prefs => {
+			workflowDir = path.join(prefs, 'workflows');
+
+			return pathExists(workflowDir);
+		})
 		.then(exists => {
 			if (!exists) {
 				throw new Error(`Workflow directory \`${workflowDir}\` does not exist`);
